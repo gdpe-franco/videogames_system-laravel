fa7f29af0a106699ac8245ba1cbb73c78baee256
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-02-23 22:39:59 -0600

    add: videogames list

--- resources/views/videogames.blade.php
+++ resources/views/videogames.blade.php
@@ -1,9 +1,14 @@
 @extends('layout')
 
-@section('title')
-    <title>Videogames</title>
-@endsection
+@section('title', 'Videogames')
 
 @section('content')
     <h1>Videogames</h1>
+    <ul>
+        @forelse($videogames as $videogame)
+            <li>{{ $videogame['title'] }}</li>
+        @empty
+            <li> There are no videogames to show </li>
+        @endforelse
+    </ul>
 @endsection
