6f037e76675c8feabe11ee718153a4beae40023a
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-02-25 08:29:18 -0600

    add: resource routes

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ use App\Http\Controllers\VideogameController;
 
 Route::view('/', 'home')-> name('home');
 Route::view('/users', 'users')-> name('users');
-Route::get('/videogames', [VideogameController::class, 'index']) -> name('videogames.index');
-Route::get('/videogames/create', [VideogameController::class, 'create']) -> name('videogames.create');
-Route::post('/videogames/', [VideogameController::class, 'store']) -> name('videogames.store');
-Route::get('/videogames/edit/{videogame}', [VideogameController::class, 'edit']) -> name('videogames.edit');
-Route::patch('/videogames/{videogame}', [VideogameController::class, 'update']) -> name('videogames.update');
-Route::delete('/videogames/{videogame}', [VideogameController::class, 'destroy']) -> name('videogames.destroy');
+
+Route::resource('videogames', VideogameController::class);
+// Route::get('/videogames', [VideogameController::class, 'index']) -> name('videogames.index');
+// Route::get('/videogames/create', [VideogameController::class, 'create']) -> name('videogames.create');
+// Route::post('/videogames/', [VideogameController::class, 'store']) -> name('videogames.store');
+// Route::get('/videogames/edit/{videogame}', [VideogameController::class, 'edit']) -> name('videogames.edit');
+// Route::patch('/videogames/{videogame}', [VideogameController::class, 'update']) -> name('videogames.update');
+// Route::delete('/videogames/{videogame}', [VideogameController::class, 'destroy']) -> name('videogames.destroy');
