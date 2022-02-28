95e59d0f14274621f88302f9220f598d21a83862
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-02-28 07:22:56 -0600

    add: new layout for auth files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,7 @@
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
+                            @yield('navmenu')
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -52,6 +53,7 @@
                                 </li>
                             @endif
                         @else
+                            @yield('navmenu')
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
