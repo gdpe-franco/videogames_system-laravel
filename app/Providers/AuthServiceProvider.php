cd46189471bab0a49540223ea81fe1eb46ac077a
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-03-10 07:51:05 -0600

    delete: unnecessary gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,7 +16,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
-        Videogame::class => VideogamePolicy::class
 
     ];
 
